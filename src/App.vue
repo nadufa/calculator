ff4e7d97b809ef76cf2a7dc0e55c6a376b68690f
Nadufa <nadufa18404@gmail.com>
2024-02-29 00:08:07 +0300

    add some logic for result validation

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="wrapper">
-    <ResultField :result='returnResultValue()'/>
-    <MyButtons :updateResultValue="updateResultValue" :valuesForButton='state.valuesForButton'/>
+    <ResultField :result='state.resultArray.join("")'/>
+    <MyButtons :updateResultValue="onButtonClick" :valuesForButton='state.valuesForButton'/>
   </div>
 </template>
 
@@ -20,92 +20,46 @@ export default {
       state: {
         resultArray: ['0'],
         valuesForButton: [
-          [
-            {
-              valueForButton: 'AC'
-            },
-            {
-              valueForButton: '+/-'
-            },
-            {
-              valueForButton: '%'
-            },
-            {
-              valueForButton: '/'
-            },
-          ],
-          [
-
-            {
-              valueForButton: '7'
-            },
-            {
-              valueForButton: '8'
-            },
-            {
-              valueForButton: '9'
-            },
-            {
-              valueForButton: '*'
-            },
-          ],
-          [
-
-            {
-              valueForButton: '4'
-            },
-            {
-              valueForButton: '5'
-            },
-            {
-              valueForButton: '6'
-            },
-            {
-              valueForButton: '-'
-            },
-          ],
-          [
-
-            {
-              valueForButton: '1'
-            },
-            {
-              valueForButton: '2'
-            },
-            {
-              valueForButton: '3'
-            },
-            {
-              valueForButton: '+'
-            },
-          ],
-          [
-            {
-              valueForButton: 'TH'
-            },
-            {
-              valueForButton: '0'
-            },
-            {
-              valueForButton: ','
-            },
-            {
-              valueForButton: '='
-            },
-          ],
+          [{valueForButton: 'AC'}, {valueForButton: '+/-'}, {valueForButton: '%'}, {valueForButton: '/'},],
+          [{valueForButton: '7'}, {valueForButton: '8'}, {valueForButton: '9'}, {valueForButton: '*'},],
+          [{valueForButton: '4'}, {valueForButton: '5'}, {valueForButton: '6'}, {valueForButton: '-'},],
+          [{valueForButton: '1'}, {valueForButton: '2'}, {valueForButton: '3'}, {valueForButton: '+'},],
+          [{valueForButton: 'TH'}, {valueForButton: '0'}, {valueForButton: ','}, {valueForButton: '='},],
         ]
       }
     }
   },
   methods: {
-    returnResultValue() {
-      return this.state.resultArray.join('')
+    onButtonClick(newSign) {
+      const newSignIsNumber = this.isItNumber(newSign)
+      if (newSign === 'AC') {
+        console.log(this.state.resultArray[this.state.resultArray.length - 1])
+        this.state.resultArray = [0]
+        console.log(this.state.resultArray[this.state.resultArray.length - 1])
+      } else if (newSign === 'TH') {
+        alert('THEME WAS CHANGED')
+      } else if (this.state.resultArray.length === 1 && this.state.resultArray[0] === '0' && newSign === '0') {
+        alert('it is already 0')
+      } else if (this.state.resultArray.length === 1 && this.state.resultArray[0] === '0' && newSignIsNumber) {
+        this.state.resultArray = [`${newSign}`]
+      } else if (!newSignIsNumber && !this.isItNumber(this.state.resultArray[this.state.resultArray.length - 1])) {
+        alert('you can not put two math signs in a row')
+      } else if (newSign === '=') {
+        if (!this.isItNumber(this.state.resultArray[this.state.resultArray.length - 1])) {
+          alert('expression should be end with a number ')
+        }
+      } else {
+        this.state.resultArray.push(newSign)
+      }
+
+
     },
-    updateResultValue(newSign) {
-      console.log(newSign)
-      this.state.resultArray.push(newSign)
-      this.returnResultValue()
+    isItNumber(sign) {
+      return sign === '0' || sign === '1' || sign === '2' || sign === '3' || sign === '4' || sign === '5' || sign === '6' || sign === '7' || sign === '8' || sign === '9'
     },
+    calculateResult(arr) {
+      console.log(arr)
+    }
   }
 }
 </script>
